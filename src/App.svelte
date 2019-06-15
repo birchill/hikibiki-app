cbc460acdd4897928e866724bf251bdd4c80e8e0
Brian Birtles <birtles@gmail.com>
2019-06-15 15:22:37 +0900

    Add initial skeleton

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { DownloadState } from './DownloadState';
+import DatabaseStatus from './DatabaseStatus.svelte';
+
+const downloadState = DownloadState.Initializing;
+</script>
+
+<DatabaseStatus {downloadState} />
---
+++ src/DownloadState.ts
@@ -0,0 +1,11 @@
+export const enum DownloadState {
+  Initializing,
+  FetchingMetadata,
+  DownloadingInitial,
+  DownloadingUpdate,
+  Updating,
+  UpToDate,
+  Offline,
+  Warning,
+  Error
+};
