3b75727b66b2c289f9b7ef0b570c16b928b3c3f4
Brian Birtles <birtles@gmail.com>
2019-09-19 20:35:08 +0900

    Split component styles out into separate files

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
