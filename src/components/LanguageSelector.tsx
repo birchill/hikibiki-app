eeb6f3c2d16aedf67d7794c4b94c5026aab12431
Brian Birtles <birtles@gmail.com>
2019-09-25 14:31:55 +0900

    Format language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -17,20 +17,29 @@ export const LanguageSelector: FunctionalComponent<Props> = (props: Props) => {
     : 'en';
 
   return (
-    <select
-      name="lang"
-      class="lang-selector"
-      onChange={evt => {
-        if (evt && evt.target && props.onSetLang) {
-          props.onSetLang((evt.target as HTMLSelectElement).value);
-        }
-      }}
-    >
-      {DB_LANGUAGES.map(lang => (
-        <option value={lang} selected={lang === selectedLang}>
-          {DB_LANGUAGE_NAMES.get(lang)!}
-        </option>
-      ))}
-    </select>
+    <div class="flex mx-auto justify-center items-center">
+      <label for="lang" class="label">
+        <svg class="w-16 h-16 mr-6 text-gray-600" viewBox="0 0 16 16">
+          <title>Language</title>
+          <use width="16" height="16" href="#globe" />
+        </svg>
+      </label>
+      <select
+        id="lang"
+        name="lang"
+        class="bg-transparent text-gray-800 text-lg cursor-pointer px-6 py-2 border border-gray-600 rounded-lg"
+        onChange={evt => {
+          if (evt && evt.target && props.onSetLang) {
+            props.onSetLang((evt.target as HTMLSelectElement).value);
+          }
+        }}
+      >
+        {DB_LANGUAGES.map(lang => (
+          <option value={lang} selected={lang === selectedLang}>
+            {DB_LANGUAGE_NAMES.get(lang)!}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
