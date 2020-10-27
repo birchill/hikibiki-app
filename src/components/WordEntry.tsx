219fb2ce2be56ec4f62e1708eeb97264aaa15522
Brian Birtles <birtles@gmail.com>
2020-10-27 11:13:59 +0900

    List localized definitions first

--- src/components/WordEntry.tsx
+++ src/components/WordEntry.tsx
@@ -312,10 +312,23 @@ function renderSenses(senses: WordResult['s'], lang: string | undefined) {
       </p>
     );
   }
+
+  const foreignSenses = senses.filter((sense) => isForeignSense(sense, lang));
+  const localSenses = senses.filter((sense) => !isForeignSense(sense, lang));
+
   return (
-    <ol class="ml-8 list-circled list-inside">
-      {senses.map((sense) => renderSense(sense, lang))}
-    </ol>
+    <Fragment>
+      {localSenses ? (
+        <ul class="ml-8 list-disc list-inside">
+          {localSenses.map((sense) => renderSense(sense, lang))}
+        </ul>
+      ) : null}
+      {foreignSenses ? (
+        <ol class="ml-8 list-circled list-inside">
+          {foreignSenses.map((sense) => renderSense(sense, lang))}
+        </ol>
+      ) : null}
+    </Fragment>
   );
 }
 
