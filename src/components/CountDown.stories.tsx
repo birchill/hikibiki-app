eb1ee31094fa896c50a44941d22b81e7c0578a8a
Brian Birtles <birtles@gmail.com>
2023-04-08 13:18:48 +0900

    chore: update to Storybook v7

--- src/components/CountDown.stories.tsx
+++ src/components/CountDown.stories.tsx
@@ -1,16 +1,25 @@
 import { h } from 'preact';
-import { storiesOf } from '@storybook/preact';
+import type { Meta, StoryObj } from '@storybook/preact';
 
 import { CountDown } from './CountDown';
 
-storiesOf('Components|CountDown', module).add('default', () => (
-  <div>
-    Now: <CountDown deadline={new Date()} />
-    <br />
-    In 10 second:{' '}
-    <CountDown deadline={new Date(new Date().getTime() + 10 * 1000)} />
-    <br />
-    In 70 seconds:{' '}
-    <CountDown deadline={new Date(new Date().getTime() + 70 * 1000)} />
-  </div>
-));
+const meta: Meta<typeof CountDown> = {
+  title: 'Components/CountDown',
+  component: CountDown,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof CountDown> = {
+  render: () => (
+    <div>
+      Now: <CountDown deadline={new Date()} />
+      <br />
+      In 10 second:{' '}
+      <CountDown deadline={new Date(new Date().getTime() + 10 * 1000)} />
+      <br />
+      In 70 seconds:{' '}
+      <CountDown deadline={new Date(new Date().getTime() + 70 * 1000)} />
+    </div>
+  ),
+};
